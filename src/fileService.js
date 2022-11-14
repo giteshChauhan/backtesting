6339236a70043f0db1a8e17909828acee577494e
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-14 18:08:55 +0530

    update my trades function

--- src/fileService.js
+++ src/fileService.js
@@ -1,3 +1,9 @@
+/** My fileServices includes methods:
+ *  fetchData() : In order to parse csv data from input file to json object. (here I used papaparse module).
+ * movingAverage() : This function is used to find moving average using past 30 days records from that date. (with O(n) time complexity ).
+ * trades() : Here I tried to simulate some trades using avgData as calculated in above function. Also I simulated only long trades (buy trades).
+ */
+
 import Papa from "papaparse";
 
 export function fetchData(id) {
@@ -49,4 +55,53 @@ export function movingAverage(id) {
   });
 
   return arr;
+}
+
+export function trades(avgData) {
+  let trades = [];
+
+  let entryPoint = null,
+    entryPrice;
+  for (let data of avgData) {
+    let currentDate = data.Date;
+    let currentPrice = parseFloat(data.Close);
+    let avgPrice = parseFloat(data.Avg);
+
+    // entryPosition :  ClosePrice <= AvgPrice
+
+    if (!entryPoint && currentPrice <= avgPrice) {
+      entryPoint = currentDate;
+      entryPrice = currentPrice;
+    }
+
+    // exitPosition
+    else if (entryPoint) {
+      let netProfit = Math.round((currentPrice - entryPrice) * 10000) / 10000;
+      let profitPercent = (netProfit / currentPrice) * 100;
+
+      // here profitPercent will ensure my stoploss which is 2%
+
+      if (currentPrice > avgPrice || profitPercent < -2.0) {
+        let exitPrice = currentPrice;
+        // As Stoploss id auto enabled hence exitPrice is different from the currentPrice.
+        if (profitPercent < -2.0) {
+          exitPrice = entryPrice - entryPrice * 0.02;
+          profitPercent = -1.999999;
+          netProfit = Math.round((exitPrice - entryPrice) * 10000) / 10000;
+        }
+
+        trades.push({
+          entryPoint,
+          exitPoint: currentDate,
+          entryPrice,
+          exitPrice,
+          netProfit,
+          profitPercent,
+        });
+        entryPoint = null; // to ensure beginning of new trade
+      }
+    }
+  }
+
+  return trades;
 }
